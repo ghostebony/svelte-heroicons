92aebd0a4a3bf9771913cfb82076d560eecf6db5
ghostebony <pedroamericolouzada@uol.com.br>
2022-12-17 18:28:34 -0300

    add builder for `class` prop type

---
+++ src/builder/types.ts
@@ -0,0 +1,27 @@
+import { lstatSync, readdirSync, readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+
+const SRC_PACKAGE_PATH = "./package";
+
+function builder(path: string = SRC_PACKAGE_PATH) {
+	for (const fileOrFolder of readdirSync(path)) {
+		const fileOrFolderPath = join(path, fileOrFolder);
+
+		if (lstatSync(fileOrFolderPath).isFile()) {
+			if (fileOrFolderPath.endsWith(".d.ts")) {
+				writeFileSync(
+					fileOrFolderPath,
+					readFileSync(fileOrFolderPath, "utf-8").replace(
+						"[x: string]: any;",
+						"class?: string;"
+					),
+					"utf-8"
+				);
+			}
+		} else {
+			builder(fileOrFolderPath);
+		}
+	}
+}
+
+builder();
